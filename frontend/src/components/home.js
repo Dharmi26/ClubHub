f7bb923dc4d0104997f7942aa64388f8ae0b1212
Dharmi26 <dharmipatel567@gmail.com>
2023-01-17 22:10:24 +0530

    image

---
+++ frontend/src/components/home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function home(){
+    return(
+        <>
+
+            {/* <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{position: 'relative'}} zindex={2}>
+                <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">SignIn</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">SignUp</a>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </nav> */}
+
+            <div style={{position: 'relative'}} zindex={1}>
+                <div style={{position: 'relative', width: '100%', height: 0, paddingTop: '56.22252%', paddingBottom: 0, marginTop: '-5em', marginBottom: '0', overflow: 'hidden', borderRadius: '8px', willChange: 'transform'}}>
+                    <iframe loading="lazy" style={{position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, border: 'none', padding: 0, margin: 0}} src="https://www.canva.com/design/DAFX7bbNO54/view?embed" allowFullScreen="allowfullscreen" allow="fullscreen">
+                    </iframe>
+                </div>
+            </div>
+
+        </>
+    )
+};
